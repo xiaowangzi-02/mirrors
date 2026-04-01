6cecf3728b8fdb725caf3687df5bd14852374834
LIMA  WILLIAM <77796989+xiaowangzi-02@users.noreply.github.com>
2026-04-01 12:36:46 +0000

    change: rosettacommons.org

--- functions/rosettacommons/[[catchall]].js
+++ functions/rosettacommons/[[catchall]].js
@@ -1,8 +1,20 @@
 export function onRequest(context) {
-  const domain = "conda.rosettacommons.org";
   const url = new URL(context.request.url);
-  url.hostname = domain;
-  url.pathname = context.functionPath.split("/").slice(2).join("/");
+  const pathnames = url.pathname.split("/");
+
+  switch (pathnames[2]) {
+    case "conda":
+      url.hostname = "conda.rosettacommons.org";
+      break;
+    case "west":
+      url.hostname = "west.rosettacommons.org";
+      break;
+    default:
+      url.hostname = "rosettacommons.org";
+      break;
+  }
+
+  url.pathname = pathnames.slice(3).join("/");
 
   return fetch(url, context.request);
 }
